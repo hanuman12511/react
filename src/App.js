4c7e55f594c7a618856edce2a62b312fe84d4a31
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-09-07 12:17:42 +0530

    wewew

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {BrowserRouter} from 'react-router-dom'
 import Router from './view/router/Router';
 import { AuthContext } from './view/context/AuthContext';
-import {data,addcart} from './view/data/data'
+import {data,addcart,addres} from './view/data/data'
 import './view/style/style.css'
 function App() {
   return(
     <>
-    <AuthContext.Provider value={{data,addcart}}>   
+    <AuthContext.Provider value={{data,addcart,addres}}>   
       <BrowserRouter>
     <Router/>
     </BrowserRouter>
--- src/view/screen/Addres.js
+++ src/view/screen/Addres.js
@@ -1,7 +1,35 @@
+import { useContext, useRef } from "react"
+import { AuthContext } from "../context/AuthContext"
+
 export default function Addres(){
+let {addres} =useContext(AuthContext)
+const name = useRef("")
+const area = useRef("")
+const city = useRef("")
+const state = useRef("")
+    function AddAddres(){
+        console.log(" addresdata");
+        const params={
+         name : name.current.value,
+            area:area.current.value, 
+            city :city.current.value,
+            state :state.current.value,
+        }
+        addres[0]=params
+        console.log(addres);
+    }
+    
     return(
         <>
-        <h1>Address</h1>
+        <div className="main-addres-div"> 
+        <div className="addres-div">
+        <input type="txt" ref={name} placeholder="Enter name"/>
+       <input type="txt" ref={area} placeholder="Enter area"/>
+       <input type="txt" ref={city} placeholder="Enter city"/>
+       <input type="txt" ref={state} placeholder="Enter state"/>
+       <button onClick={AddAddres}>Add Addres</button>
+       </div>
+       </div>
         </>
     )
 }
